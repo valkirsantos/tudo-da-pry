1bd3f7d0aa73d50bc93032eaa5be3ee38c9875a8
Valkir <valkirsantos@gmail.com>
2026-04-24 17:30:11 -0300

    correcoes bugs tela vendedor

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => array_filter(array_unique([
+        'http://localhost:5173',
+        'http://localhost:8000',
+        env('FRONTEND_URL'),
+    ])),
 
     'allowed_origins_patterns' => [],
 
